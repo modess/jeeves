771620be3bd8cfb129df3887280144ed9bd06091
Niklas Modess <niklas@modess.io>
2018-02-16 07:20:11 +0100

    hello world

---
+++ src/GenerateCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Jeeves;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('generate');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello world');
+    }
+}
